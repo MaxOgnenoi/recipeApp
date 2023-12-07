bf2540a2377ceae2c2365a477b4cb3398418bae9
Max Ognenoi <maksymohnenyi>
2023-12-06 22:11:25 -0500

    update8

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
@@ -7,17 +7,22 @@ import { BellIcon } from 'react-native-heroicons/outline';
 export default function HomeScreen() {
     return (
         <View className="flex-1 bg-white">
-            <StatusBar style='dark' />
+            <StatusBar style="dark" />
             <ScrollView
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{ paddingBottom: 50 }}
                 className="space-y-6 pt-14">
                 {/* avatar and bell icon */}
                 <View className="mx-4 flex-row justify-between items-center mb-2">
-                    <Image source={require('../../assets/images/avatar.png')} style={{ height: (5), width: hp(5.5) }} />
+                    <Image source={require('../../assets/images/avatar.png')} style={{ height: hp(5), width: hp(5.5) }} />
                     <BellIcon size={hp(4)} color="gray" />
                 </View>
-            </ScrollView>
-        </View>
+
+                {/* greetings and punchline*/}
+                <View className="mx-4 space-y-2 mb-2">
+                    <Text style={{ fontSize: hp(1.7) }} className="text-neutral-600">Hello, Max!</Text>
+                </View>
+            </ScrollView >
+        </View >
     )
 }

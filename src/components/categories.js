9c6ad90ae11216fc1473a0ecfe306e3f23b41f4f
Max Ognenoi <maksymohnenyi>
2023-12-08 23:29:07 -0500

    updaste18

--- src/components/categories.js
+++ src/components/categories.js
@@ -11,10 +11,11 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import Animated, { FadeInDown } from "react-native-reanimated";
 
 export default function Categories(activeCategory, setActiveCategory) {
   return (
-    <View>
+    <Animated.View entering={FadeInDown.duration(500).springify()}>
       <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
@@ -44,6 +45,6 @@ export default function Categories(activeCategory, setActiveCategory) {
           );
         })}
       </ScrollView>
-    </View>
+    </Animated.View>
   );
 }

b8af4907d02e7a8165a5f510e31da62838f6d18d
Max Ognenoi <maksymohnenyi>
2023-12-08 23:15:13 -0500

    update16

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,10 +1,43 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  ScrollView,
+  Touchable,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { categoryData } from "../constants";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function Categories() {
-    return (
-        <View>
-            <Text>Categories</Text>
-        </View>
-    )
+  return (
+    <View>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className="space-x-4"
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+      >
+        {categoryData.map((cat, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              className="flex items-center space-y-1"
+            >
+              <View className="rounded-full p-[6px]">
+                <Image
+                  source={{ uri: cat.image }}
+                  style={{ width: hp(6), height: hp(6) }}
+                  className="rounded-full"
+                />
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
 }

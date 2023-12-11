ecca3af913d458c16734ca10ffd2a1fdda9231b7
Max Ognenoi <maksymohnenyi>
2023-12-10 22:53:40 -0500

    update24

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -1,2 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
+
+export default function Recipes() {
+  return (
+    <View>
+      <Text>Recipes</Text>
+    </View>
+  );
+}

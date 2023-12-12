584710e3cf21f9262f196c4c7068282633165a91
Max Ognenoi <maksymohnenyi>
2023-12-11 23:08:32 -0500

    update30

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,10 +13,12 @@ import Recipes from "../components/recipes";
 export default function HomeScreen() {
   const [activeCategory, setActiveCategory] = useState("Beef");
   const [categories, setCategories] = useState([]);
+  const [meals, setMeals] = useState([]);
 
   useEffect(() => {
     getCategories();
-  });
+    getRecipes();
+  }, []);
 
   const getCategories = async () => {
     try {
@@ -26,6 +28,19 @@ export default function HomeScreen() {
       //console.log("got categories: ", response.data);
       if (response && response.data) {
         setCategories(response.data.categories);
+      }
+    } catch (err) {
+      console.log("error: ", err.message);
+    }
+  };
+  const getRecipes = async (category = "Beef") => {
+    try {
+      const response = await axios.get(
+        `https://themealdb.com/api/json/v1/1/filter.php?c=${category}`
+      );
+      //console.log("got recipes: ", response.data);
+      if (response && response.data) {
+        setMeals(response.data.meals);
       }
     } catch (err) {
       console.log("error: ", err.message);

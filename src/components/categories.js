b4baaa04be42f7a9b1d3e35e22d28660899149ae
Max Ognenoi <maksymohnenyi>
2023-12-15 23:51:05 -0500

    update43

--- src/components/categories.js
+++ src/components/categories.js
@@ -36,8 +36,8 @@ export default function Categories({
               className="flex items-center space-y-1"
             >
               <View className={"rounded-full p-[6px]" + activeButtonClass}>
-                <Image
-                  source={{ uri: cat.strCategoryThumb }}
+                <CashedImage
+                  uri={cat.strCategoryThumb}
                   style={{ width: hp(6), height: hp(6) }}
                   className="rounded-full"
                 />

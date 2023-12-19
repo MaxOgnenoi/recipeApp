b361abb7aa833d140c6ec365a60307e8944659fc
Max Ognenoi <maksymohnenyi>
2023-12-18 23:19:55 -0500

    update50

--- src/screens/RecipeDetailScreen.js
+++ src/screens/RecipeDetailScreen.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { CashedImage } from "../helpers/image";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import { ChevronLeftIcon } from "vue-feather-icons";
 
 export default function RecipeDetailScreen(props) {
   let item = props.route.params;
@@ -24,11 +25,18 @@ export default function RecipeDetailScreen(props) {
             width: wp(98),
             height: hp(50),
             borderRadius: 53,
-            borderBottomLeftRaduius: 40,
-            borderBottomRightRaduius: 40,
+            borderBottomLeftRadius: 40,
+            borderBottomRightRadius: 40,
             marginTop: 4,
           }}
         />
+      </View>
+
+      {/* back button */}
+      <View className="w-full absolute flex-row justify-between items-center pt-14">
+        <TouchableOpacity className="p-2 rounded-full ml-5 ng-white">
+          <ChevronLeftIcon size={hp(3.5)} strokeWidth={4.5} color="#fbbf24" />
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );

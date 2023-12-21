7d963dbb5d0cdd75e22c98d57a4c541f33eee09e
Max Ognenoi <maksymohnenyi>
2023-12-20 23:39:55 -0500

    update58

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, ActivityIndicator } from "react-native";
 
-export default function Loading() {
+export default function Loading(props) {
   return (
     <View className="flex-1 flex justify-center items-center">
       <ActivityIndicator {...props} />

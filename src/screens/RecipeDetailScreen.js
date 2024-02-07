42ed0795f3180d41e132777455593fe693d4e13f
Max Ognenoi <maksymohnenyi>
2024-02-06 23:33:35 -0500

    update71

--- src/screens/RecipeDetailScreen.js
+++ src/screens/RecipeDetailScreen.js
@@ -265,32 +265,10 @@ export default function RecipeDetailScreen(props) {
             >
               Instructions
             </Text>
-            <View className="space-y-2 ml-3">
-              {ingredientsIndexes(meal).map((i) => {
-                return (
-                  <View key={i} className="flex-row space-x-4">
-                    <View
-                      style={{ height: hp(1.5), width: hp(1.5) }}
-                      className="bg-amber-300 rounded-full"
-                    />
-                    <View className="flex-row space-x-2">
-                      <Text
-                        style={{ fontSize: hp(1.7) }}
-                        className="font-extrabold text-neutral-700"
-                      >
-                        {meal["strMeasure" + i]}
-                      </Text>
-                      <Text
-                        style={{ fontSize: hp(1.7) }}
-                        className="font-medium text-neutral-600"
-                      >
-                        {meal["strIngredient" + i]}
-                      </Text>
-                    </View>
-                  </View>
-                );
-              })}
-            </View>
+            <Text
+              style={{ fontSize: hp(1.6) }}
+              className="text-neutral-700"
+            ></Text>
           </View>
         </View>
       )}

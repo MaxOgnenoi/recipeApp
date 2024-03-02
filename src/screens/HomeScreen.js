dfd845258b459928168e5a9e265608d5a2f6475e
Max Ognenoi <maksymohnenyi>
2024-03-02 18:50:20 -0500

    update94

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,342 +1,144 @@
+import { View, Text, ScrollView, Image, TextInput } from "react-native";
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  ScrollView,
-  StatusBar,
-} from "react-native";
-import { CachedImage } from "../helpers/image";
+import { StatusBar } from "expo-status-bar";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import {
-  ChevronLeftIcon,
-  ClockIcon,
-  UsersIcon,
-  FireIcon,
-  Square3Stack3DIcon,
-} from "react-native-heroicons/outline";
-import { HeartIcon } from "vue-feather-icons";
-import { useNavigation } from "@react-navigation/native";
+import { BellIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import Categories from "../components/categories";
 import axios from "axios";
-import Loading from "../components/loading";
-import YoutubeIframe from "react-native-youtube-iframe";
-import Animated, { FadeInDown } from "react-native-reanimated";
+import Recipes from "../components/recipes";
 
-export default function RecipeDetailScreen(props) {
-  console.log("props.route.params:", props.route.params);
+export default function HomeScreen() {
+  const [activeCategory, setActiveCategory] = useState("Beef");
+  const [categories, setCategories] = useState([]);
 
-  let item = props.route.params;
-  const [isFavourite, setIsFavourite] = useState(false);
-  const navigation = useNavigation();
-  const [meal, setMeal] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [meals, setMeals] = useState([]);
 
   useEffect(() => {
-    getMealData(item.idMeal);
+    getCategories();
+    getRecipes();
   }, []);
 
-  const getMealData = async (id) => {
+  const handleChangeCategory = (category) => {
+    getRecipes(category);
+    setActiveCategory(category);
+    setMeals([]);
+  };
+
+  const getCategories = async () => {
     try {
       const response = await axios.get(
-        `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+        "https://themealdb.com/api/json/v1/1/categories.php"
       );
+
       if (response && response.data) {
-        setMeal(response.data.meals[0]);
-        setLoading(false);
+        setCategories(response.data.categories);
       }
     } catch (err) {
       console.log("error: ", err.message);
     }
   };
 
-  const ingredientsIndexes = (meal) => {
-    if (!meal) return [];
-    let indexes = [];
-    for (let i = 1; i <= 20; i++) {
-      indexes.push(i);
-    }
-    return indexes;
-  };
+  const getRecipes = async (category = "Beef") => {
+    try {
+      const response = await axios.get(
+        `https://themealdb.com/api/json/v1/1/filter.php?c=${category}`
+      );
 
-  const getYoutubeVideoId = (url) => {
-    const regex = /[?&]v=([^&]+)/;
-    const match = url.match(regex);
-    if (match && match[1]) {
-      return match[1];
+      if (response && response.data) {
+        setMeals(response.data.meals);
+      }
+    } catch (err) {
+      console.log("error: ", err.message);
     }
-    return null;
   };
 
   return (
-    <ScrollView
-      contentContainerStyle={{ paddingBottom: hp(5) }}
-      style={{ backgroundColor: "white" }}
-    >
-      <StatusBar style="light" />
-      <View style={{ flexDirection: "row", justifyContent: "center" }}>
-        <CachedImage
-          uri={item.strMealThumb}
-          sharedTransitionTag={item.strMeal}
-          style={{
-            width: wp(98),
-            height: hp(50),
-            borderRadius: 53,
-            borderBottomLeftRadius: 40,
-            borderBottomRightRadius: 40,
-            marginTop: 4,
-          }}
-        />
-      </View>
-
-      <Animated.View
-        entering={FadeInDown.delay(200).duration(1000)}
-        style={{
-          width: "100%",
-          position: "absolute",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          paddingTop: hp(14),
-        }}
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        style={{ paddingTop: hp(14) }}
       >
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-          style={{
-            padding: hp(2),
-            borderRadius: hp(2),
-            marginLeft: wp(5),
-            backgroundColor: "white",
-          }}
-        >
-          <ChevronLeftIcon size={hp(3.5)} strokeWidth={4.5} color="#fbbf24" />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => setIsFavourite(!isFavourite)}
-          style={{
-            padding: hp(2),
-            borderRadius: hp(2),
-            marginRight: wp(5),
-            backgroundColor: "white",
-          }}
-        >
-          <HeartIcon
-            size={hp(3.5)}
-            strokeWidth={4.5}
-            color={isFavourite ? "red" : "grey"}
-          />
-        </TouchableOpacity>
-      </Animated.View>
-
-      {loading ? (
-        <Loading size="large" style={{ marginTop: hp(16) }} />
-      ) : (
+        {/* avatar and bell icon */}
         <View
           style={{
-            paddingHorizontal: wp(4),
+            marginHorizontal: 4,
+            flexDirection: "row",
             justifyContent: "space-between",
-            paddingTop: hp(8),
+            alignItems: "center",
+            marginBottom: 2,
           }}
         >
-          <Animated.View
-            entering={FadeInDown.duration(700).springify().damping(12)}
-            style={{ marginBottom: hp(4) }}
-          >
-            <Text
-              style={{ fontSize: hp(3), fontWeight: "bold", color: "#333" }}
-            >
-              {meal?.strMeal}
-            </Text>
-            <Text style={{ fontSize: hp(2), color: "#666" }}>
-              {meal?.strArea}
-            </Text>
-          </Animated.View>
+          <Image
+            source={require("../../assets/images/avatar.png")}
+            style={{ height: hp(5), width: hp(5.5) }}
+          />
+          <BellIcon size={hp(4)} color="gray" />
+        </View>
 
-          <Animated.View
-            entering={FadeInDown.delay(100)
-              .duration(700)
-              .springify()
-              .damping(12)}
-            style={{ flexDirection: "row", justifyContent: "space-around" }}
+        {/* greetings and punchline*/}
+        <View style={{ marginHorizontal: 4, marginBottom: 2 }}>
+          <Text style={{ fontSize: hp(1.7), color: "gray" }}>Hello, Max!</Text>
+          <Text
+            style={{ fontSize: hp(3.8), fontWeight: "bold", color: "gray" }}
           >
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                borderRadius: hp(2),
-                backgroundColor: "#FFD700",
-                padding: hp(2),
-              }}
-            >
-              <ClockIcon size={hp(4)} strokeWidth={2.5} color="#525252" />
-              <View style={{ marginLeft: wp(2) }}>
-                <Text
-                  style={{ fontSize: hp(2), fontWeight: "bold", color: "#333" }}
-                >
-                  35
-                </Text>
-                <Text style={{ fontSize: hp(1.3), color: "#333" }}>Mins</Text>
-              </View>
-            </View>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                borderRadius: hp(2),
-                backgroundColor: "#FFD700",
-                padding: hp(2),
-              }}
-            >
-              <UsersIcon size={hp(4)} strokeWidth={2.5} color="#525252" />
-              <View style={{ marginLeft: wp(2) }}>
-                <Text
-                  style={{ fontSize: hp(2), fontWeight: "bold", color: "#333" }}
-                >
-                  03
-                </Text>
-                <Text style={{ fontSize: hp(1.3), color: "#333" }}>
-                  Servings
-                </Text>
-              </View>
-            </View>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                borderRadius: hp(2),
-                backgroundColor: "#FFD700",
-                padding: hp(2),
-              }}
-            >
-              <FireIcon size={hp(4)} strokeWidth={2.5} color="#525252" />
-              <View style={{ marginLeft: wp(2) }}>
-                <Text
-                  style={{ fontSize: hp(2), fontWeight: "bold", color: "#333" }}
-                >
-                  103
-                </Text>
-                <Text style={{ fontSize: hp(1.3), color: "#333" }}>Cal</Text>
-              </View>
-            </View>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                borderRadius: hp(2),
-                backgroundColor: "#FFD700",
-                padding: hp(2),
-              }}
-            >
-              <Square3Stack3DIcon
-                size={hp(4)}
-                strokeWidth={2.5}
-                color="#525252"
-              />
-              <View style={{ marginLeft: wp(2) }}>
-                <Text
-                  style={{ fontSize: hp(2), fontWeight: "bold", color: "#333" }}
-                ></Text>
-                <Text style={{ fontSize: hp(1.3), color: "#333" }}>Easy</Text>
-              </View>
-            </View>
-          </Animated.View>
-
-          <Animated.View
-            entering={FadeInDown.delay(200)
-              .duration(700)
-              .springify()
-              .damping(12)}
-            style={{ marginBottom: hp(4) }}
+            Make your own food,
+          </Text>
+          <Text
+            style={{ fontSize: hp(3.8), fontWeight: "bold", color: "gray" }}
           >
-            <Text
-              style={{ fontSize: hp(2.5), fontWeight: "bold", color: "#333" }}
-            >
-              Ingredients
-            </Text>
-            <View style={{ marginLeft: wp(4), marginTop: hp(2) }}>
-              {ingredientsIndexes(meal).map((i) => {
-                return (
-                  <View
-                    key={i}
-                    style={{ flexDirection: "row", alignItems: "center" }}
-                  >
-                    <View
-                      style={{
-                        width: hp(1.5),
-                        height: hp(1.5),
-                        backgroundColor: "#FFD700",
-                        borderRadius: hp(0.75),
-                      }}
-                    />
-                    <View style={{ flexDirection: "row", marginLeft: wp(2) }}>
-                      <Text
-                        style={{
-                          fontSize: hp(1.7),
-                          fontWeight: "bold",
-                          color: "#333",
-                        }}
-                      >
-                        {meal["strMeasure" + i]}
-                      </Text>
-                      <Text
-                        style={{
-                          fontSize: hp(1.7),
-                          color: "#666",
-                          marginLeft: wp(2),
-                        }}
-                      >
-                        {meal["strIngredient" + i]}
-                      </Text>
-                    </View>
-                  </View>
-                );
-              })}
-            </View>
-          </Animated.View>
+            stay at <Text style={{ color: "#FFC400" }}>home</Text>
+          </Text>
+        </View>
 
-          <Animated.View
-            entering={FadeInDown.delay(300)
-              .duration(700)
-              .springify()
-              .damping(12)}
-            style={{ marginBottom: hp(4) }}
+        {/* search bar */}
+        <View
+          style={{
+            marginHorizontal: 4,
+            flexDirection: "row",
+            alignItems: "center",
+            borderRadius: 20,
+            backgroundColor: "rgba(0,0,0,0.05)",
+            padding: 6,
+          }}
+        >
+          <TextInput
+            placeholder="Search any recipe"
+            placeholderTextColor={"gray"}
+            style={{
+              flex: 1,
+              fontSize: hp(1.7),
+              marginBottom: 1,
+              paddingLeft: 3,
+            }}
+          />
+          <View
+            style={{ backgroundColor: "white", borderRadius: 20, padding: 3 }}
           >
-            <Text
-              style={{ fontSize: hp(2.5), fontWeight: "bold", color: "#333" }}
-            >
-              Instructions
-            </Text>
-            <Text
-              style={{ fontSize: hp(1.6), color: "#333", marginTop: hp(1) }}
-            >
-              {meal?.strInstructions}
-            </Text>
-          </Animated.View>
+            <MagnifyingGlassIcon size={hp(2.5)} strokeWidth={3} color="gray" />
+          </View>
+        </View>
 
-          {meal.strYoutube && (
-            <Animated.View
-              entering={FadeInDown.delay(400)
-                .duration(700)
-                .springify()
-                .damping(12)}
-              style={{ marginBottom: hp(4) }}
-            >
-              <Text
-                style={{ fontSize: hp(2.5), fontWeight: "bold", color: "#333" }}
-              >
-                Recipe Video
-              </Text>
-              <View>
-                <YoutubeIframe
-                  videoId={getYoutubeVideoId(meal.strYoutube)}
-                  height={hp(30)}
-                />
-              </View>
-            </Animated.View>
+        {/* categories */}
+        <View>
+          {categories.length > 0 && (
+            <Categories
+              categories={categories}
+              activeCategory={activeCategory}
+              handleChangeCategory={handleChangeCategory}
+            />
           )}
         </View>
-      )}
-    </ScrollView>
+
+        {/* recipes */}
+        <View>
+          <Recipes meals={meals} categories={categories}></Recipes>
+        </View>
+      </ScrollView>
+    </View>
   );
 }
